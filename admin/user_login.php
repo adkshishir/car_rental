bbd3c0f55ee7eda8c99949b97eadc28a8393968e
dkc1549 <dkc1549@gmail.com>
2023-06-02 22:37:53 +0545

    User login page ma authentication garaya ko

--- admin/user_login.php
+++ admin/user_login.php
@@ -18,11 +18,27 @@
                 <?php
                 if (isset($_POST['submit'])) {
                     $usrname = $_POST['username'];
-                    $pass = $_POST['password'];
+                    $password = $_POST['password'];
+                    $password=md5($password);
+                    $emailCheck = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+                    $emailCount = mysqli_query($connect, $emailCheck);
+                    $arr = $emailCount->fetch_assoc();
+                    $count = mysqli_num_rows($emailCount);
+                    if ($count == 1) {
+                      session_start();
+                      $_SESSION['email'] = $arr['email'];
+                      $_SESSION['id'] = $arr['uid'];
+                      ?>
+                      <script>
+                        location.replace("index.php");
+                      </script>
+                      <?php
+                 
+                    }
                 }
                 ?>
                 <div class="email">
-                    <label for="email">Enter your username:
+                    <label for="email">Enter your email:
                     </label>
                     <input type="text" name="username" id="email">
                 </div>
