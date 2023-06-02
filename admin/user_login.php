f457aea2bc99ee4b2d1f6c477e968abc39e9e445
dkc1549 <dkc1549@gmail.com>
2023-06-02 14:38:36 +0545

    Admin folder ma user login page ko starting garako

---
+++ admin/user_login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Login page</title>
+</head>
+<body>
+    <header>
+        <h2>Enter your details</h2>
+    </header>
+    <main>
+        <?php
+            if(isset($_POST['submit'])){
+                $usrname=$_POST['username'];
+                $pass=$_POST['password'];
+            }
+        ?>
+    <section>
+        <form action="#" method="post">
+            <div class="email">
+            <label for="email">Enter your username:
+            </label>
+            <input type="text" name="username" id="email">
+            </div>
+            <div class="pass">
+                <label for="password">Enter your password:</label>
+                <input type="password" name="password" id="password">
+            </div>
+        </form>
+    </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
