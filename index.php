64829a69598989958c4a22c76b84c1c87b7dbfde
dkc1549 <dkc1549@gmail.com>
2023-06-04 10:10:52 +0545

    index page css

--- index.php
+++ index.php
@@ -1,14 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CarRental</title>
-</head>
-<body>
-    <header>
-        
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ezy Rentals</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+    <link rel="stylesheet" href="asserts/css/style.css" />
+  </head>
+  <body>
+    <header class="main-header">
+      <center>
+        <h1 class="title">Ezy Rentals</h1>
+
+        <nav class="nav nav-bar">
+          <ul class="nav nav-ul nav-sticky">
+            <li class="nav-element">Home</li>
+            <li class="nav-element">home</li>
+            <li class="nav-element">Home</li>
+            <li class="nav-element">Home</li>
+          </ul>
+        </nav>
+      </center>
     </header>
-</body>
+    <main class="main">
+      <h1 class="car-collection">some category</h1>
+      <section class="car-collection">
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+      </section>
+      <h1 class="car-collection">some category</h1>
+
+      <section class="car-collection">
+        <div class="car-detail">
+          <img src="" />
+          <h1>photo</h1>
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+      </section>
+      <section></section>
+      <h1 class="car-collection">some category</h1>
+      <section class="car-collection">
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+        <div class="car-detail">
+          <img src="" />
+          <h2>name</h2>
+          <h3>brand</h3>
+          <h3>price per day</h3>
+        </div>
+      </section>
+    </main>
+    <footer class="footer">
+      <div>
+        <nav class="footer-nav">
+          <a href="www.facebook.com">facebook</a>
+          <a href="www.youtube.com"> youtube</a>
+          <a href="www.insgram.com"> instagram</a>
+        </nav>
+      </div>
+      <iframe
+        frameborder="0"
+        src="https://www.google.com/maps/embed/v1/place?q=Sydney+NSW,+Australia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+      ></iframe>
+    </footer>
+  </body>
 </html>
